e24f6e35d2e0412c8aa0011d6fca1beb74dc6e21
jlzhou <jlzhou@sycapital.com.cn>
2020-04-30 17:13:06 +0800

    update time

--- docs/.vuepress/config/pluginConf.js
+++ docs/.vuepress/config/pluginConf.js
@@ -6,7 +6,7 @@ module.exports = {
     '@vuepress/last-updated':
     {
         transformer: (timestamp) => {
-            return moment(timestamp).format("LLLL")
+            return moment(timestamp).utc(8).format("LLLL")
         }
     },
     '@vuepress/pwa': {
